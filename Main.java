3af35dc1b7406badb40e9b2c43500e684ec604b6
joannasegerstroom-del <joanna.segerstroom@gmail.com>
2025-12-15 01:16:07 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    //Main-metod som startar spelet
+    public static void main(String[] args) {
+    DragonTreasure game = new DragonTreasure();
+    game.setupGame();
+    }
+}
---
+++ Player.java
@@ -0,0 +1,15 @@
+public class Player {
+    
+    //Instansvariabel
+    private String name;
+
+    //Konstruktor
+    public Player(String name) {
+        this.name = name;
+    }
+
+    //"Getter"- Metod som returnerar spelarens namn
+    public String getName() {
+        return name;
+    }
+}

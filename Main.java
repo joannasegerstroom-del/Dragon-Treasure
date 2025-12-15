c57e6535829e8af3adf352127df34ed2fc71ede7
joannasegerstroom-del <joanna.segerstroom@gmail.com>
2025-12-15 03:23:49 +0100

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    //Main-metod som startar spelet
-    public static void main(String[] args) {
-    DragonTreasure game = new DragonTreasure();
-    game.setupGame();
-    }
-}
